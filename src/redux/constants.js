06480d8d8253eda0d6a6c64894bbafd3299b8d17
mauricioinaz <mauricio.glez@gmail.com>
2019-07-24 15:02:11 -0400

    VERSION 1.02

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -52,7 +52,7 @@ export const SPANISH_DATA = {
     aboutTitle: "Sobre nuestra radio",
     aboutInfo: "Radio Ach’ Lequilc’op es una radio comunitaria pertinente e integrada a la realidad de las comunidades de la Selva Norte de Chiapas. Sirve como sistema educativo, informativo y de concientización, para contribuir a una vida en armonía en la región, conformada por tseltales y mestizos. Además del equipo base, más de 30 personas dan su tiempo voluntario para traer la palabra de las diferentes regiones y procesos de las comunidades, brindando así un servicio a nuestro pueblo.",
     aboutApp: "Desarrollamos esta app pensando en nuestros radioescuchas que están fuera de su comunidad, estado o país. Que con ella llevemos en nuestro bolsillo, donde quiera que estemos, los contenidos y horarios de Ach’ Lequilc’op, y un pequeño recordatorio de nuestra cultura Tseltal.",
-    appVersion: "Version 1.01",
+    appVersion: "Version 1.02",
     aboutAck: "Coordinadora:  Amalia Hernández\nDesarrollo:  Mauricio González\n\
 Traducción:  Francisco Guzmán\nDiseño Logo:  Aquila Diseño\nEquipo Radio:  Gilberto Miranda y Ángel López"
   },
@@ -92,7 +92,7 @@ export const TSELTAL_DATA = {
     aboutInfo: "Te Ach' Lequilc'op C'opojibal ha' jchahp c'opojibal te ha' stuc soc melel ya x'a'tej ta ts'acal ta bin ay sc'oblal yu'un te jaychahp comonal ban c'alal ya smac ta squ'inal Chiapas. \
 Ha' yac ta tuquinel binut'il jtojobteswanej, yaqu'el ta na'el smelelil c'op soc nix yac'beyel swic' sit jaytuhl tseltal soc jcaxlan ants winic. Ha' nix hich chapal ta jaytuhl j'a'tel ta yutil te c'opojibal soc nix c'axen wan ta lajuneb scha'winic xan ta tuhl jcoltaywanejetic te ya yac' ya'telic ta yich'el talel c'op ban c'alal jujun smacoj squ'inalic soc sc'op jaychahp a'tel ay ta jujun comonal, yu'un hich melel ay yaqu'el bah abatinel ta yutil jlumaltic.",
     aboutApp: "Laj jchahpancotic ha'i a'tel ini ta sc'oblal jaytuhl mach'atic ma' nacal ta slumalic, chican teme li' ta yutil nix squ'inal Chiapas ay ma'uc teme ayic ta namal. Yu'un hich xhu' bayuc ban ayotic, ya jchupojtic ta jujutuhl bin a'tel ya xlaj pasaluc soc nix na'alotic ah bintic ya xlaj c'ax ta Ach' Lequilc'op C'opojibal, ha' nix hich jchahp sna'ojibal te banti loqu'em talel jts'umbaltic te ho'otic melel tseltalotique.",
-    appVersion: "Version 1.01",
+    appVersion: "Version 1.02",
     aboutAck: "Jolal ta c'opojibal:  Amalia Hernández\nSchahpanej te a'tejibal:  Mauricio González\n\
 Sohltesej ta tseltal c'op:  Francisco Guzmán\nRadio:  Gilberto Miranda soc j'Angel López"
   },
